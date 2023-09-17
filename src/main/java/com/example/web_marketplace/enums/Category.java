e1f44925581db2f832319b8c269771e3b2f3193b
Admin <tukavolody@gmail.com>
2023-09-17 16:55:57 +0300

    Add enum

---
+++ src/main/java/com/example/web_marketplace/enums/Category.java
@@ -0,0 +1,12 @@
+package com.example.web_marketplace.enums;
+
+public enum Category {
+    FOOD,
+    TECHNICS,
+    CLOTHES,
+    SPORTS_COMMODITY,
+    CHILDREN_COMMODITY,
+    BUILDING_MATERIALS,
+    FURNITURE,
+    HOUSEHOLD_APPLIANCES,
+}
