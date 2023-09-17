e36c65a8bbff31465e880a68273c7c26464f44b3
Admin <tukavolody@gmail.com>
2023-09-17 16:55:49 +0300

    Add exception

---
+++ src/main/java/com/example/web_marketplace/exceptions/BadRequestException.java
@@ -0,0 +1,11 @@
+package com.example.web_marketplace.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException{
+    public BadRequestException(String massage){
+        super(massage);
+    }
+}
