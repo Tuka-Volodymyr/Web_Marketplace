b0ae98bfb1eac6f9766763d0e255e1d344d80b74
Admin <tukavolody@gmail.com>
2023-09-17 17:00:16 +0300

    Add main class

---
+++ src/main/java/com/example/web_marketplace/WebMarketplace.java
@@ -0,0 +1,11 @@
+package com.example.web_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMarketplace {
+    public static void main(String[] args){
+        SpringApplication.run(WebMarketplace.class, args);
+    }
+}
