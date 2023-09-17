fd8c34a700e6ac908ebcbd083eab2d56a5403635
Admin <tukavolody@gmail.com>
2023-09-17 16:55:35 +0300

    Add entity class

---
+++ src/main/java/com/example/web_marketplace/entities/Goods.java
@@ -0,0 +1,31 @@
+package com.example.web_marketplace.entities;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.time.LocalDate;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Goods {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name should not be blank!")
+    private String name;
+    @NotBlank(message = "Description should not be blank!")
+    private String description;
+    @NotBlank(message = "Category should not be blank!")
+    private String category;
+    @NumberFormat
+    private long price;
+    private LocalDate localDate;
+    private String userEmail;
+
+}
---
+++ src/main/java/com/example/web_marketplace/entities/User.java
@@ -0,0 +1,43 @@
+package com.example.web_marketplace.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Account")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name should not be blank!")
+    @Size(min = 4,max = 20, message = "Name length is wrong!")
+    private String fullName;
+
+    @NotBlank(message = "Email should not be blank!")
+    @Email(message = "It isn`t email format!")
+    private String email;
+
+    @NotBlank(message = "Password should not be blank!")
+    private String password;
+
+    @NotBlank(message = "Phone should not be blank!")
+    @Size(min = 10,max = 13,message = "Phone length is wrong!")
+    private String phone;
+
+    public String toString(){
+        return "Name: "+getFullName()+
+                "\nEmail: "+getEmail()+
+                "\nPhone: "+getPhone();
+    }
+
+
+}
