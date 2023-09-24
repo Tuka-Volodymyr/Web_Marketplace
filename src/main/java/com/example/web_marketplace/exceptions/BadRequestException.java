7a9f23fdd88ed16dd862cc507ca534803b1e91b3
Admin <tukavolody@gmail.com>
2023-09-24 16:13:58 +0300

    Update exception

--- src/main/java/com/example/web_marketplace/exceptions/BadRequestException.java
+++ src/main/java/com/example/web_marketplace/exceptions/BadRequestException.java
@@ -3,8 +3,10 @@ package com.example.web_marketplace.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.function.Supplier;
+
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-public class BadRequestException extends RuntimeException{
+public class BadRequestException extends RuntimeException {
     public BadRequestException(String massage){
         super(massage);
     }
