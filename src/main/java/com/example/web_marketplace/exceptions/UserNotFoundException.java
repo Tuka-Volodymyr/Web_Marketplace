829786bcb6cc47251b4f5f8d0e90d473c2ef3f95
Admin <tukavolody@gmail.com>
2023-09-24 16:16:24 +0300

    Update exception

--- src/main/java/com/example/web_marketplace/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/web_marketplace/exceptions/UserNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.web_marketplace.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND,reason = "User don`t exist!")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST,reason = "User don`t exist!")
 public class UserNotFoundException extends RuntimeException{
 }
