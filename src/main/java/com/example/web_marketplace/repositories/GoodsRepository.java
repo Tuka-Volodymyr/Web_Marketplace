6268ff2ea8545b16c9201c517cc5769a462b0c21
Admin <tukavolody@gmail.com>
2023-09-29 21:54:28 +0300

    Update interface

--- src/main/java/com/example/web_marketplace/repositories/GoodsRepository.java
+++ src/main/java/com/example/web_marketplace/repositories/GoodsRepository.java
@@ -1,6 +1,7 @@
 package com.example.web_marketplace.repositories;
 
 import com.example.web_marketplace.entities.Goods;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,16 +10,17 @@ import java.util.Optional;
 
 @Repository
 public interface GoodsRepository extends JpaRepository<Goods,Long> {
-    Optional<Goods> findByNameAndUserEmail(String name, String email);
+    Optional<Goods> findByIdAndUserEmail(Long id, String userEmail);
     List<Goods> findByCategory(String category);
-    List<Goods> findByPriceGreaterThanEqual(long minPrice);
+    List<Goods> findByPriceGreaterThanEqual(long minPrice, Sort sort);
     List<Goods> findByPriceLessThanEqual(long maxPrice);
-    List<Goods> findByPriceGreaterThanEqualAndPriceLessThanEqual(long minPrice,long maxPrice);
+    List<Goods> findByPriceGreaterThanEqualAndPriceLessThanEqual(long minPrice,long maxPrice, Sort sort);
     List<Goods> findByCategoryAndPriceLessThanEqual(String category,long maxPrice);
-    List<Goods> findByUserEmail(String category);
+    List<Goods> findByUserEmail(String category, Sort sort);
 
 
 
-    List<Goods> findByCategoryAndPriceGreaterThanEqual(String category,long price);
-    List<Goods> findByCategoryAndPriceGreaterThanEqualAndPriceLessThanEqual(String category,long minPrice,long maxPrice);
+
+    List<Goods> findByCategoryAndPriceGreaterThanEqual(String category,long price, Sort sort);
+    List<Goods> findByCategoryAndPriceGreaterThanEqualAndPriceLessThanEqual(String category,long minPrice,long maxPrice, Sort sort);
 }
