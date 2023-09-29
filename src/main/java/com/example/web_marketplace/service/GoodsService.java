795c1a42024bd432b2210240a7d16b8ac5e8a18b
Admin <tukavolody@gmail.com>
2023-09-29 21:53:46 +0300

    Update service

--- src/main/java/com/example/web_marketplace/service/GoodsService.java
+++ src/main/java/com/example/web_marketplace/service/GoodsService.java
@@ -2,10 +2,11 @@ package com.example.web_marketplace.service;
 
 import com.example.web_marketplace.data.GoodsData;
 import com.example.web_marketplace.data.UserData;
-import com.example.web_marketplace.dto.FilterForm;
+import com.example.web_marketplace.forms.FilterForm;
 import com.example.web_marketplace.entities.Goods;
 import com.example.web_marketplace.entities.User;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ import java.time.LocalDate;
 public class GoodsService {
     private final GoodsData goodsData;
     private final UserData userData;
+    public static final Sort sort = Sort.by(Sort.Direction.ASC, "price");
     public GoodsService(GoodsData goodsData, UserData userData) {
         this.userData = userData;
         this.goodsData = goodsData;
@@ -28,34 +30,41 @@ public class GoodsService {
         goods.setUserEmail(userDetails.getUsername());
         goodsData.save(goods);
     }
-    public String deleteGoods(String name){
+    public void deleteGoods(long idGoods){
         UserDetails userDetails=getUserDetails();
         User user = userData.findByEmail(userDetails.getUsername());
-        goodsData.delete(goodsData.findByNameAndSellerID(name,userDetails.getUsername()));
-        return name+" was deleted!";
+        goodsData.delete(goodsData.findByIdAndSellerEmail(idGoods,userDetails.getUsername()));
     }
     public void getGoods(Model model){
-       model.addAttribute("goods",goodsData.findAll());
+       model.addAttribute("goods",goodsData.findAll(sort));
     }
     public void getYourGoods(Model model){
         UserDetails userDetails=getUserDetails();
-        model.addAttribute("goods",goodsData.findByEmail(userDetails.getUsername()));
+        model.addAttribute("goods",goodsData.findByEmail(userDetails.getUsername(),sort));
     }
     public void getFoundGoodsWithFilter(Model model, FilterForm filter){
+
         if (!filter.getCategory().isBlank() && filter.getMaxPrice()> filter.getMinPrice()) {
-            model.addAttribute("goods",goodsData.findByMinPriceAndCategoryAndMaxPrice(filter));
+            model.addAttribute("goods",goodsData.findByMinPriceAndCategoryAndMaxPrice(filter,sort));
         }else if (!filter.getCategory().isBlank() ) {
-            model.addAttribute("goods", goodsData.findByMinPriceAndCategory(filter));
+            model.addAttribute("goods", goodsData.findByMinPriceAndCategory(filter,sort));
         } else if (filter.getMaxPrice() > filter.getMinPrice()) {
-            model.addAttribute("goods",goodsData.findByMinPriceAndMax(filter));
+            model.addAttribute("goods",goodsData.findByMinPriceAndMax(filter,sort));
         }else if (filter.getMinPrice() > 0){
-            model.addAttribute("goods",goodsData.findByMinPrice(filter.getMinPrice()));
+            model.addAttribute("goods",goodsData.findByMinPrice(filter.getMinPrice(),sort));
 
         }
     }
+    public void getSortDecreaseByPrice(Model model){
+//        model.addAttribute("goods",)
+
+    }
+    public void getSortIncreaseByPrice(Model model){
 
 
-    private UserDetails getUserDetails() {
+    }
+
+    public static UserDetails getUserDetails() {
         return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
--- src/main/java/com/example/web_marketplace/service/UserService.java
+++ src/main/java/com/example/web_marketplace/service/UserService.java
@@ -1,12 +1,12 @@
 package com.example.web_marketplace.service;
 
 import com.example.web_marketplace.data.CodeData;
+import com.example.web_marketplace.data.GoodsData;
 import com.example.web_marketplace.data.UserData;
-import com.example.web_marketplace.dto.ChangePass;
+import com.example.web_marketplace.forms.ChangePass;
 import com.example.web_marketplace.entities.Code;
 import com.example.web_marketplace.entities.User;
 import com.example.web_marketplace.exceptions.BadRequestException;
-import com.example.web_marketplace.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -17,77 +17,53 @@ import org.springframework.ui.Model;
 
 @Service
 public class UserService {
+    private final GoodsData goodsData;
     private final UserData userData;
     private final PasswordEncoder passwordEncoder;
     private final JavaMailSender javaMailSender;
     private final CodeData codeData;
-    public UserService(UserData userData, PasswordEncoder passwordEncoder, CodeData codeData, JavaMailSender javaMailSender) {
+    public UserService(UserData userData, PasswordEncoder passwordEncoder, CodeData codeData, JavaMailSender javaMailSender, GoodsData goodsData) {
         this.passwordEncoder=passwordEncoder;
         this.userData = userData;
         this.codeData=codeData;
         this.javaMailSender = javaMailSender;
+        this.goodsData = goodsData;
     }
-    public String addUser(User user, Model model){
-        try {
-            if(userData.isExist(user.getEmail()))throw new BadRequestException(
-                    "Email has already used!"
-            );
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userData.save(user);
-            return "authorization/login";
-        } catch (BadRequestException e) {
-            model.addAttribute("error", e.getMessage());
-            return "authorization/register";
-        }
+    public void addUser(User user){
+        if(userData.isExist(user.getEmail()))throw new BadRequestException(
+                "Email has already used!"
+        );
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userData.save(user);
     }
-    public String infoOfUser(String email, Model model){
-        try {
-            model.addAttribute("user", userData.findByEmail(email));
-            return "account/userInfo";
-        } catch (BadRequestException e) {
-            model.addAttribute("error", e.getMessage());
-            return "account/emailForm";
-        }
+    public void infoOfUser(String email, Model model){
+        model.addAttribute("user", userData.findByEmail(email));
     }
-    public String sendCode(String email, Model model){
-        try {
-            User user= userData.findByEmail(email);
-            if(codeData.findCodeByAccountId(user.getId()).isPresent())
-                codeData.deleteCode(codeData.findCodeByAccountId(user.getId()).get());
-            Code code=new Code(user.getId());
-            codeData.saveCode(code);
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(email);
-            message.setSubject("Password Reset");
-            message.setText("Code: "+code.getCode());
-            javaMailSender.send(message);
-            return "redirect:/get/check/code";
-        } catch (BadRequestException e) {
-            model.addAttribute("error", e.getMessage());
-            return "account/changePassword/emailFormForChangePass";
-        }
+    public void goodsToAttribute(Model model,String email){
+
+        model.addAttribute("goods",goodsData.findByEmail(email,GoodsService.sort));
     }
-    public String checkCode(Code code,Model model){
-        try {
-            codeData.findByCode(code.getCode());
-            return "redirect:/get/change/password";
-        }catch(Exception e){
-            model.addAttribute("error",e.getMessage());
-            return "account/changePassword/codeForm";
-        }
+    public void sendCode(String email){
+        User user= userData.findByEmail(email);
+        if(codeData.findCodeByAccountId(user.getId()).isPresent())
+            codeData.delete(codeData.findCodeByAccountId(user.getId()).get());
+        Code code=new Code(user.getId());
+        codeData.save(code);
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Password Reset");
+        message.setText("Code: "+code.getCode());
+        javaMailSender.send(message);
     }
-    public String changePass(ChangePass changePass,Model model){
-        try {
-            User user =userData.findByEmail(changePass.getEmail());
-            if(!changePass.getConfirmPassword().equals(changePass.getPassword()))
-                throw new BadRequestException("Password don`t match");
-            user.setPassword(passwordEncoder.encode(changePass.getPassword()));
-            userData.save(user);
-            return "redirect:/";
-        }catch(Exception e){
-            model.addAttribute("error",e.getMessage());
-            return "account/changePassword/changePassword";
-        }
+    public void checkCode(Code code){
+        codeData.findByCode(code.getCode());
+    }
+    public void changePass(ChangePass changePass){
+        User user =userData.findByEmail(changePass.getEmail());
+        if(!changePass.getConfirmPassword().equals(changePass.getPassword()))
+            throw new BadRequestException("Password don`t match");
+        user.setPassword(passwordEncoder.encode(changePass.getPassword()));
+        userData.save(user);
     }
 
 }
